c1a7eaff545b4cefdb9023c2b83d940a2c1a6688
princessruthie <missruthie@gmail.com>
2020-01-28 20:26:12 -0500

    inital commit

---
+++ canvas-submit.py
@@ -0,0 +1,46 @@
+# Import the Canvas class
+from canvasapi import Canvas
+from canvasapi.assignment import Assignment
+from canvasapi.requester import Requester
+import config #please don't ever show anyone your key. they'd be able to submit on your behalf
+
+#This is provided with NO WARRANTY WHATSOEVER.
+#1) If your computer explodes, I take no responsibility.
+#2) If your dog bites you and your cat leaves you, it's not my problem.
+#3) If you use this code and all your stuff uploads to my private server, it's deeply unintentional.
+#4) In all honesty, I'd be tickled if it helped anyone at all.
+
+### You may wish to change this
+# your file name of choice
+FILE = "submission.zip"
+### Everything below is unlikely to change
+
+# Canvas API URL
+API_URL = "https://canvas.upenn.edu/api/v1/"
+# Canvas API key
+API_KEY = config.API_KEY
+#HW01 Part01
+ASSIGNMENT_ID = 7454616
+#596 course id
+COURSE_ID = 1488863
+
+
+
+# Initialize a new Requester
+requester = Requester(API_URL, API_KEY)
+
+# Make an Assignment for this course and this assignment
+assignment = Assignment(requester, {'course_id': COURSE_ID, 'id': ASSIGNMENT_ID})
+# print(assignment.to_json())
+
+#if you go into the plain REST api, you'll see the python library is combining two steps:
+#   1) upload a file named whatever you named yours
+#   2) submit the assignment using the id of canvas id for the uploaded file in 1
+response = assignment.submit({
+    'submission_type': 'online_upload',
+    }, FILE
+)
+
+print(response)
+print("nothing eventful happened but you should always check the site")
+
---
+++ config-sample.py
@@ -0,0 +1,4 @@
+#to use this file, copy/rename it to config.py and put in your own API_KEY value
+
+API_KEY = "paste that super long jawn here"
+USER_ID = 123456 #I ended up not using this but this is where you could keep it private
